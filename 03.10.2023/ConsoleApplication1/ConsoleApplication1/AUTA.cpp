7e404a205b14cc757b3e16d8b11fb2173f80d77d
Fuksoll <145668476+Fuksoll@users.noreply.github.com>
2023-10-03 14:58:29 +0200

    AUTA
    auta

---
+++ 03.10.2023/ConsoleApplication1/ConsoleApplication1/AUTA.cpp
@@ -0,0 +1,138 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x;
+	cout << "Napisz liczbe od 1 do 30: " << endl;
+	cin >> x;
+	
+	switch (x) 
+	{
+
+	case 1:
+		cout << "Audi";
+		break;
+
+	case 2:
+		cout << "Mercedes";
+		break;
+
+	case 3:
+		cout << "BMW";
+		break;
+
+	case 4:
+		cout << "Mazda";
+		break;
+
+	case 5:
+		cout << "Ford";
+		break;
+
+	case 6:
+		cout << "Toyota";
+		break;
+
+	case 7:
+		cout << "Honda";
+		break;
+
+	case 8:
+		cout << "Hyundai";
+		break;
+
+	case 9:
+		cout << "Mitsubishi";
+		break;
+
+	case 10:
+		cout << "Nissan";
+		break;
+
+	case 11:
+		cout << "Jeep";
+		break;
+
+	case 12:
+		cout << "Passat";
+		break;
+
+	case 13:
+		cout << "Subaru";
+		break;
+
+	case 14:
+		cout << "Volvo";
+		break;
+
+	case 15:
+		cout << "Fiat";
+		break;
+
+	case 16:
+		cout << "Mini";
+		break;
+
+	case 17:
+		cout << "Bugatii";
+		break;
+
+	case 18:
+		cout << "Citroen";
+		break;
+
+	case 19:
+		cout << "Ferrari";
+		break;
+
+	case 20:
+		cout << "Lambo";
+		break;
+
+	case 21:
+		cout << "Bentley";
+		break;
+
+	case 22:
+		cout << "Maseratii";
+		break;
+
+	case 23:
+		cout << "Lexus";
+		break;
+
+	case 24:
+		cout << "Porshe";
+		break;
+
+	case 25:
+		cout << "Jaguar";
+		break;
+
+	case 26:
+		cout << "Maklini";
+		break;
+
+	case 27:
+		cout << "Marek Marucha";
+		break;
+
+	case 28:
+		cout << "Aston Martin";
+		break;
+
+	case 29:
+		cout << "Alfa Romeo";
+		break;
+
+	case 30:
+		cout << "Lande ROverek";
+	break;
+
+	default:
+		cout << "Nieprawidłowa liczba"; break;
+	}
+	return 0;
+}
