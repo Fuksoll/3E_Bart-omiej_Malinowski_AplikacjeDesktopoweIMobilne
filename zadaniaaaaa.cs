8298135935aeec9cb4dad33a969ef887f5a2835d
CEZET <145668476+Fuksoll@users.noreply.github.com>
2024-10-02 14:10:09 +0200

    Add files via upload

---
+++ zadaniaaaaa.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.ComponentModel.Design;
+public class zadaniaaa
+{
+    public static void Main(string[] args)
+    {
+        /*
+         Z1
+        var px = 0;
+        var py = 0;
+
+        if (px == 0 || py == 0)
+        {
+            Console.WriteLine("rowna sie 0");
+        }
+        else if (px > 0 && py > 0)
+        {
+            Console.WriteLine("jest rowne 1");
+        }
+        else if (px < 0 && py < 0)
+        {
+            Console.WriteLine("jest rowne 3");
+        }
+        else if (px > 0 && py < 0) {
+            Console.WriteLine("jest rowna 4");
+        }
+        else if (px < 0 && py > 0) {
+            Console.WriteLine("jest rowne 2");
+        }*/
+
+
+        /*
+         Z2
+        int ab = 1;
+        int ca = 2;
+        int bc = 3; //najdluzszy
+        double obw = ab + ca + bc;
+        double h = (ab + Math.Sqrt(3)) / 2;
+        double p = ab * h / 2;
+
+        if (bc < ab + ca)
+        {
+            Console.WriteLine("mozna zrobic trojkat");
+        }
+        else if (bc > ab + ca)
+        {
+            Console.WriteLine("nie mozna zrobic trojkata");
+        }
+        Console.WriteLine("obwod rowna sie:" + obw);
+        Console.WriteLine("pole rowna sie:" + p);*/
+
+
+        /*
+         z4
+        Console.WriteLine("podaj liczbe 1");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("podaj liczbe 2");
+        int b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("podaj symbol dzialania jeśli + = 1, - = 2, * = 3, / = 4");
+        int c = Convert.ToInt32(Console.ReadLine());
+        
+        if (c == 1)
+        {
+            Console.WriteLine(a + b);
+        }
+        else if (c == 2)
+        {
+            Console.WriteLine(a - b);
+        }
+        else if (c == 3)
+        {
+             Console.WriteLine(a * b);
+        }
+        else if (c == 4)
+        { 
+            Console.WriteLine(a / b); 
+        }
+        else if (c <= 0 || c > 4)
+        {
+            Console.WriteLine("blad nie wybrano dzialania");
+        }
+        
+
+        switch (c)
+        {
+            case 1: Console.WriteLine(a + b);
+                break;
+            case 2: Console.WriteLine(a - b); 
+                break;
+            case 3: Console.WriteLine(a * b); 
+                break;
+            case 4: Console.WriteLine(a / b);
+                break;
+            default: Console.WriteLine("blad nie wybrano dzialania");
+                break;
+        }
+        */
+    }
+}
